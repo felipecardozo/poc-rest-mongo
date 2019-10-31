e0f6289d2306ed1730d0d0e5b592e5f6462f5d9c
Felipe Cardozo <fcardozo@rccl.com>
2019-10-31 18:47:28 -0500

    base project

---
+++ src/main/java/com/oportunit/demo/PocRestMongoApplication.java
@@ -0,0 +1,13 @@
+package com.oportunit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRestMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocRestMongoApplication.class, args);
+	}
+
+}
